ed6f8c388f55f9e5b48b33d1b282dd4acec1f03f
mylesholloway1 <mylesholloway1@gmail.com>
2022-09-01 12:05:11 -0400

    update

--- ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
+++ ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
@@ -1,17 +1,11 @@
 import boto3
 cli = boto3.client('ec2', region_name='us-east-1')
-res = cli.describe_instances(
-    Filters=[
+res = cli.instances.filter(
+    InstanceIds=[
         {
-            'Name': 'Instance-id',
+            'Name': 'InstanceId',
             'Values': [
                 'i-07121ff5aecbc50e1',
-            ],
-        },
-        {
-            'Name': 'location',
-            'Values': [
-                'us-east-1',
             ],
         },
     ],
