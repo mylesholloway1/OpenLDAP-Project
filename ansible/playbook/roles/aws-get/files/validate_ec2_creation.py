4af48ad048f4169d7f9381ce3440e47989e986a4
mylesholloway1 <mylesholloway1@gmail.com>
2022-09-01 14:49:48 -0400

    update

--- ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
+++ ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
@@ -5,7 +5,7 @@ ec2_region    = sys.argv[1]
 instance_name = sys.argv[2]
 
 ec2 = boto3.client('ec2', region_name=ec2_region)
-res = cli.describe_instances(
+res = ec2.describe_instances(
     Filters=[
         {
             'Name': 'tag:Name',
