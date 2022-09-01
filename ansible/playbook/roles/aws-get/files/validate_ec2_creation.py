0c7d3f7de4a8335c5cac043f0d27a9a4a87515d2
mylesholloway1 <mylesholloway1@gmail.com>
2022-09-01 14:17:58 -0400

    update

--- ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
+++ ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
@@ -3,6 +3,8 @@ import boto3
 
 ec2_region    = sys.argv[1]
 instance_name = sys.argv[2]
+print(ec2_region)
+print(instance_name)
 
 cli = boto3.client('ec2', region_name=ec2_region)
 res = cli.describe_instances(
