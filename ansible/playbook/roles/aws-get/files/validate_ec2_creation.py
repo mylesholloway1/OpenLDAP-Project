bc584fc0da80aaf1829f35e1f636c3ed67e97f60
mylesholloway1 <mylesholloway1@gmail.com>
2022-09-01 11:52:30 -0400

    update

--- ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
+++ ansible/playbook/roles/aws-get/files/validate_ec2_creation.py
@@ -1,16 +1,13 @@
 import boto3
-print(boto3.__version__)
-
-
-#cli = boto3.client('ec2')
-#res = cli.describe_instances(
-#    Filters=[
-#        {
-#            'Name': 'Instance-id',
-#            'Values': [
-#                'i-07121ff5aecbc50e1',
-#            ],
-#        },
-#    ],
-#)
-#print(res)
+cli = boto3.client('ec2')
+res = cli.describe_instances(
+    Filters=[
+        {
+            'Name': 'Instance-id',
+            'Values': [
+                'i-07121ff5aecbc50e1',
+            ],
+        },
+    ],
+)
+print(res)
